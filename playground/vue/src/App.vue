1829f8de470bdf31c3efdb8c878aeab1523e27e2
jens <jens@doellmann.com>
2022-05-03 11:57:28 +0200

    add spin

--- playground/vue/src/App.vue
+++ playground/vue/src/App.vue
@@ -4,12 +4,12 @@
   <section class="max-w-6xl mx-auto px-6 py-12">
     <h1 class="text-5xl font-bold">Icons</h1>
     <section class="mt-6 flex space-x-2">
-      <div class="i-heroicons-solid-trash w-8 h-8 hover:text-green-500"></div>
+      <div class="i-heroicons-solid-trash w-8 h-8 hover:text-cyan-500"></div>
       <div
         class="i-heroicons-outline-lock-closed w-8 h-8 hover:i-heroicons-outline-lock-open"
       ></div>
       <div class="bg-heroicons-solid-trash-green-500 w-8 h-8"></div>
-      <div class="i-custom-loading w-8 h-8"></div>
+      <div class="i-custom-loading w-8 h-8 animate-spin text-orange-600"></div>
       <div class="i-openmoji-cat w-8 h-8"></div>
     </section>
   </section>
