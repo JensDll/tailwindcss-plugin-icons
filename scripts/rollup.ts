5ffa0bb9258492870cad7c456c1775d1a83e44ca
jens <jens@doellmann.com>
2022-09-07 21:01:15 +0200

    return anonymous function from resolve extensions

--- scripts/rollup.ts
+++ scripts/rollup.ts
@@ -1,7 +1,7 @@
 import type { ResolverFunction } from '@rollup/plugin-alias'
 
 export function resolveExtensions(extensions: string[]): ResolverFunction {
-  return async function customResolver(source) {
+  return async function (source) {
     for (const extension of extensions) {
       try {
         const moduleInfo = await this.load({ id: source + extension })
