66de847ecef2563c3bff177b57648749d9a66730
jens <jens@doellmann.com>
2022-11-15 10:42:11 +0100

    use `clearMocks` over `mockReset` in vitest config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,16 +1,15 @@
 /// <reference types="vitest" />
 
 import path from 'node:path'
-import { fileURLToPath } from 'node:url'
 
 import { defineConfig } from 'vitest/config'
 
-const rootDir = fileURLToPath(new URL('.', import.meta.url))
+import { rootDir } from './scripts/utils'
 
 export default defineConfig({
   test: {
     globals: true,
-    mockReset: true
+    clearMocks: true
   },
   resolve: {
     alias: [
