ad199931b4ed7b207645143cf1007c452e3973d0
jens <jens@doellmann.com>
2023-03-23 14:36:23 +0100

    remove vitest inline deps

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,10 +5,7 @@ import { tsPathAlias } from './scripts/rollup'
 export default defineConfig({
   test: {
     globals: true,
-    clearMocks: true,
-    deps: {
-      inline: ['tailwindcss/lib/util']
-    }
+    clearMocks: true
   },
   resolve: {
     alias: [tsPathAlias]
