213d5bfd9d2807deb3dba496599ec040ea85203a
jens <jens@doellmann.com>
2023-12-05 10:44:30 +0100

    coverage only from packages

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,10 @@ export default defineConfig({
   test: {
     globals: true,
     clearMocks: true,
+    coverage: {
+      extension: 'ts',
+      include: ['packages/'],
+    },
   },
   resolve: {
     alias: [packagesAlias, scriptsAlias],
